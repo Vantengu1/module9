bc3ea7b13eafd4a85e2a5424e8376f507f1954fb
Dmitriy Yaryshev <krestovcode@gmail.com>
2024-09-11 20:18:18 +0300

    added

---
+++ index.js
@@ -0,0 +1,10 @@
+const submit = document.getElementById('button');
+const form = document.getElementById('form');
+
+async function openForm() {}
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  form.getElementsByClassName('form:active');
+  openForm();
+});
